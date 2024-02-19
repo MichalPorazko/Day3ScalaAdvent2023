63a128b493ebb3d061cbe0aaee6d31059467673b
MichalPorazko <michal.porazko@gmail.com>
2024-02-19 18:43:58 +0100

    Day 3 creating project

--- src/main/scala/Task3.scala
+++ src/main/scala/Task3.scala
@@ -1,12 +1,85 @@
-import caseClases.Point
+import scala.io.Source
 
 object Task3 {
 
-  /*val singOnLeft: PartialFunction[String, Boolean] =
-    case   */
 
-  def isPartNumber(schematic: String, firstDigitPointer: Int, digitsLength: Int): Boolean =
-    ???
+  //this functions reads the engine schematic from the file and flattens it as an 1D grid
+  def readingFromFile(filePath: String, state: Task3): Unit = 
+    val stringBuilder = new StringBuilder
+    var rowCount = 0
+
+    val fileSource = Source.fromFile(filePath)
+    try {
+      for (line <- fileSource.getLines()) {
+        rowCount += 1
+        if (state.rowLength == 0) {
+          state.rowLength = line.length
+        }
+        stringBuilder.append(line.trim)
+      }
+    } finally {
+      fileSource.close()
+    }
+
+    state.input = stringBuilder.toString()
+    state.rowCount = rowCount
+
+ 
+
+  def fixedEngine(input: String, index: Int = 0, state: Task3, rowLength: Int): Int =
+    if (index >= input.length)
+      state.sum
+    else
+      val character = input(index)
+      if (input(index).isDigit && !state.isNumberDetected)
+        state.number += input(index)
+        state.firstDigitPointer = index
+        fixedEngine(input, index + 1, state, rowLength)
+        ??? 
+      else if (input(index).isDigit && state.isNumberDetected)
+        state.number += input(index)
+        fixedEngine(input, index + 1, state, rowLength)
+      else
+        state.number = ""
+
+
+  def haha(schematic: String, firstDigitPointer: Int, rowLength: Int): Boolean = {
+    val startCoordinates = getCoordinates(firstDigitPointer, rowLength)
+    val numberLength = state.number.length
+    val endCoordinates = getCoordinates(firstDigitPointer + numberLength - 1, rowLength)
+
+    // List of potential adjacent positions relative to the number
+    val adjacentOffsets = List((-1, -1), (-1, 0), (-1, 1), (0, -1), (0, 1), (1, -1), (1, 0), (1, 1))
+
+    adjacentOffsets.exists { case (rowOffset, colOffset) =>
+      val adjacentRow = startCoordinates._1 + rowOffset
+      val adjacentCol = startCoordinates._2 + colOffset
+
+      // Check boundaries
+      if (adjacentRow >= 0 && adjacentRow < schematic.length / rowLength &&
+        adjacentCol >= 0 && adjacentCol < rowLength) {
+        val pointer = getPointer(rowLength, (adjacentRow, adjacentCol))
+        val character = schematic(pointer)
+
+        // Check if the character is a symbol
+        character match {
+          case '*' | '+' | '#' | '$' => true
+          case _ => false
+        }
+      } else {
+        false
+      }
+    }
+  }
+
+
+  def isPartNumber(schematic: String, firstDigitPointer: Int, rowLength: Int): Boolean =
+    val firstDigitCoordinates = getCoordinates(firstDigitPointer, rowLength)
+    if (schematic.length > 1)
+      val lastDigitCoordinates = getCoordinates(firstDigitPointer + (schematic.length - 1), rowLength)
+
+
+
 
   def getCoordinates(pointer: Int, rowLength: Int): (Int, Int) =
     (pointer/rowLength, pointer%rowLength)
@@ -15,7 +88,16 @@ object Task3 {
     rowLength * coordinates._1 + coordinates._2
 
 
-
-
-
 }
+
+class Task3(
+             var sum: Int = 0,
+             var number: String = "",
+             var firstDigitPointer: Int = 0,
+             var isNumberDetected: Boolean = false,
+             var input: String = "",
+             var rowLength: Int = 0,
+             var rowCount: Int = 0
+           )
+
+
--- src/test/scala/Test.scala
+++ src/test/scala/Test.scala
@@ -1,13 +1,17 @@
 class Test extends munit.FunSuite{
 
-  test("testing getCoordinates method"){
+  test("testing getCoordinates method with simple 1D and 2D grids"){
+
+    //1 x 1
     assertEquals(Task3.getCoordinates(0, 1), (0,0))
 
+    //2 x 2
     assertEquals(Task3.getCoordinates(0, 2), (0,0))
     assertEquals(Task3.getCoordinates(1, 2), (0,1))
     assertEquals(Task3.getCoordinates(2, 2), (1,0))
     assertEquals(Task3.getCoordinates(3, 2), (1,1))
 
+    //3 x 3
     assertEquals(Task3.getCoordinates(0, 3), (0, 0))
     assertEquals(Task3.getCoordinates(1, 3), (0, 1))
     assertEquals(Task3.getCoordinates(2, 3), (0, 2))
@@ -17,7 +21,6 @@ class Test extends munit.FunSuite{
     assertEquals(Task3.getCoordinates(6, 3), (2, 0))
     assertEquals(Task3.getCoordinates(7, 3), (2, 1))
     assertEquals(Task3.getCoordinates(8, 3), (2, 2))
-
   }
   
   test("testing getPointer method"){
@@ -26,5 +29,54 @@ class Test extends munit.FunSuite{
     assertEquals(Task3.getPointer(2, (1,0)), 2)
     assertEquals(Task3.getPointer(2, (1,1)), 3)
   }
+
+
+  test("testing getCoordinates method with edge cases - left top"){
+    //egde situation the number is on the left top corner and it's length is 3, the row length is 5
+    //example
+    // 1 2 3 x
+    // x x x x
+    assertEquals(Task3.getCoordinates(3, 5), (0, 3))
+    assertEquals(Task3.getCoordinates(5, 5), (1, 0))
+    assertEquals(Task3.getCoordinates(6, 5), (1, 1))
+    assertEquals(Task3.getCoordinates(7, 5), (1, 2))
+    assertEquals(Task3.getCoordinates(8, 5), (1, 3))
+  }
+
+  test("testing getCoordinates method with edge cases - left top") {
+    //egde situation the number is on the left top corner and it's length is 3, the row length is 5
+    //example
+    // 1 2 3 x
+    // x x x x
+    assertEquals(Task3.getCoordinates(3, 5), (0, 3))
+    assertEquals(Task3.getCoordinates(5, 5), (1, 0))
+    assertEquals(Task3.getCoordinates(6, 5), (1, 1))
+    assertEquals(Task3.getCoordinates(7, 5), (1, 2))
+    assertEquals(Task3.getCoordinates(8, 5), (1, 3))
+  }
+
+  test("testing getCoordinates method with edge cases - right top") {
+    //egde situation the number is on the right top corner and it's length is 2, the row length is 6
+    //example
+    // x 3 3
+    // x x x
+    assertEquals(Task3.getCoordinates(3, 6), (0, 3))
+    assertEquals(Task3.getCoordinates(9, 6), (1, 3))
+    assertEquals(Task3.getCoordinates(10, 6), (1, 4))
+    assertEquals(Task3.getCoordinates(11, 6), (1, 5))
+  }
+
+  test("testing getCoordinates method with edge cases - right top") {
+    //egde situation the number is on the right top corner and it's length is 2, the row length is 6
+    //example
+    // x 3 3
+    // x x x
+    assertEquals(Task3.getCoordinates(3, 6), (0, 3))
+    assertEquals(Task3.getCoordinates(9, 6), (1, 3))
+    assertEquals(Task3.getCoordinates(10, 6), (1, 4))
+    assertEquals(Task3.getCoordinates(11, 6), (1, 5))
+  }
+
+
 
 }
